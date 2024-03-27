215ccaf1269e0457bfe586b1d3a37058a1645faf
alexeytihonoff <alexeytihonoff@yandex.ru>
2024-03-27 09:26:36 +0700

    update task5

--- lesson-01/05.js
+++ lesson-01/05.js
@@ -29,3 +29,37 @@ switch (true) {
         console.log("Что-то пошло не так");
         break;
 }
+
+rule = "Еще не родился тот челове";
+
+switch (true) {
+    case rule.length < 25:
+        console.log("Все таки нет правил без исключения");
+        break;
+    case rule.length > 25:
+        console.log("У меня 100 пудов все получится");
+        break;
+    case rule.length === 25:
+        console.log("50 на 50");
+        break;
+    default:
+        console.log("Что-то пошло не так");
+        break;
+}
+
+rule = "Еще не родился тот челов";
+
+switch (true) {
+    case rule.length < 25:
+        console.log("Все таки нет правил без исключения");
+        break;
+    case rule.length > 25:
+        console.log("У меня 100 пудов все получится");
+        break;
+    case rule.length === 25:
+        console.log("50 на 50");
+        break;
+    default:
+        console.log("Что-то пошло не так");
+        break;
+}
