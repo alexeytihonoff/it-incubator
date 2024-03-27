53451cde9996b886afe0153b4341e80c62220b91
alexeytihonoff <alexeytihonoff@yandex.ru>
2024-03-27 09:17:57 +0700

    update task5

--- lesson-01/05.js
+++ lesson-01/05.js
@@ -22,7 +22,10 @@ switch (true) {
     case rule.length > 25:
         console.log("У меня 100 пудов все получится");
         break;
-    default:
+    case rule.length === 25:
         console.log("50 на 50");
+        break;
+    default:
+        console.log("Что-то пошло не так");
         break;
 }
