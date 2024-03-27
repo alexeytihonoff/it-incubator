6dd69edfe2ff07faf0d7059f8819f7d5530432c2
alexeytihonoff <alexeytihonoff@yandex.ru>
2024-03-27 08:34:57 +0700

    add task3

---
+++ lesson-01/03.js
@@ -0,0 +1,11 @@
+// Имея в распоряжении только:
+// let number = 1;
+// и используя лишь
+// --number   number--  ++number   number++
+// Получи в консоли 1,3,3,1
+
+let number = 1;
+console.log(number++);
+console.log(++number);
+console.log(number--);
+console.log(--number);
