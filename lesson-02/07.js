69ac561c13a49f6b7e5e951ed07d44d9741c053d
alexeytihonoff <alexeytihonoff@yandex.ru>
2024-04-18 16:17:05 +0700

    update task7 lesson2

--- lesson-02/07.js
+++ lesson-02/07.js
@@ -8,6 +8,7 @@ let passportMarried = {
 };
 
 let passportMarried2 = { ...passportMarried, married: true};
+passportMarried2.married = true;
 
 console.log(passportMarried);
 console.log(passportMarried2);
