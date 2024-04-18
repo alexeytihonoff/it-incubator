f76ea9cd3c0d81a6371a68adf1f9bd869505d761
alexeytihonoff <alexeytihonoff@yandex.ru>
2024-04-18 15:58:57 +0700

    add task6 lesson2

---
+++ lesson-02/06.js
@@ -0,0 +1,14 @@
+let passportWithAddress = {
+    name: "Petr",
+    surname: "Petrov",
+    address: {
+        country: "USA",
+        city: "LA"
+    }
+};
+
+let copyPassportWithAddress = structuredClone(passportWithAddress);
+copyPassportWithAddress.address.city = "Bobryisk";
+
+console.log(passportWithAddress.address.city);
+console.log(copyPassportWithAddress.address.city);
