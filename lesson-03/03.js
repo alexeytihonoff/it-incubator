95272db72ea710173462a0f910fd79413e4a451c
alexeytihonoff <alexeytihonoff@yandex.ru>
2024-04-22 11:47:15 +0700

    update task3 lesson3

--- lesson-03/03.js
+++ lesson-03/03.js
@@ -1,3 +1,5 @@
 const fruits = ["яблоко", "банан", "апельсин", "манго", "киви", "ананас", "виноград", "груша", "слива", "персик"];
 
-console.log(fruits.slice(1, -2))
+let arrSlice = fruits.slice(1, -2);
+
+console.log(arrSlice);
