b9cc7b41f842251c74fc6a0797be966603c7d68c
alexeytihonoff <alexeytihonoff@yandex.ru>
2024-04-22 10:37:01 +0700

    update taskq lessone

--- lesson-03/01.js
+++ lesson-03/01.js
@@ -1,1 +1,8 @@
 // code
+const colors = ["синий", "черный", "зеленый", "красный", "желтый", "зеленый"];
+
+function createColorString() {
+    return colors.filter(value => value === "черный" || value === "красный" || value === "желтый").join('-')
+}
+
+console.log(createColorString());
